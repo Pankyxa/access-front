c5d749aafa32e9939af30e1308d5ea00c634831c
Ekaterina Chalkova <tch4cat@yandex.ru>
2024-04-28 15:10:54 +0500

    Add confirm .vue

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from "@/components/auth/Login.vue";
 import Requests from "@/components/Requests.vue";
+import Confirm from "@/components/confirmation/Confirm.vue";
 
 const router = createRouter ({
   history: createWebHistory(),
   routes: [
     { path: '/', component: Login},
-    {path: '/requests', component: Requests}
+    {path: '/requests', component: Requests},
+    {path: '/confirm', component: Confirm}
   ]
 })
 
