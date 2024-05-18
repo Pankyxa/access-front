eea257e9c215b604c1edd2967d43b32693e73129
Pankyxa <kbelozerov1@example.com>
2024-05-18 23:38:20 +0500

    +blocked user

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,52 +1,58 @@
 <template>
-  <v-dialog v-bind="dialog">
-    <v-card>
-      <v-card-text>Вы были заблокировны. Если вас заблокировали по ошибке, обратитесь к системному администратору</v-card-text>
-      <v-card-actions>
-        <v-btn
-          color="green darken-1"
-          @click="closeTab"
-        >Ок</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
-  <v-container class="fill-height" fluid>
-    <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="4">
+  <v-app>
+    <v-main>
+      <v-dialog v-model="dialog" :width="600">
         <v-card class="elevation-12">
-          <v-toolbar color="primary" dark flat>
-            <v-toolbar-title>Вход</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form @submit.prevent="login">
-              <v-text-field
-                v-model="email"
-                label="Email"
-                prepend-icon="mdi-account"
-                type="email"
-              ></v-text-field>
-              <v-text-field
-                v-model="password"
-                label="Пароль"
-                prepend-icon="mdi-lock"
-                type="password"
-              ></v-text-field>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" type="submit">Войти</v-btn>
-              </v-card-actions>
-            </v-form>
+          <v-card-text>Вы были заблокированы. Если вас заблокировали по ошибке, обратитесь к системному администратору
           </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" @click="dialog = false">Ок</v-btn>
+          </v-card-actions>
         </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+      </v-dialog>
+
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Вход</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form @submit.prevent="login">
+                  <v-text-field
+                    v-model="email"
+                    label="Email"
+                    prepend-icon="mdi-account"
+                    type="email"
+                    :rules="[rules.required, rules.email]"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    label="Пароль"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    :rules="[rules.required]"
+                  ></v-text-field>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" type="submit">Войти</v-btn>
+                  </v-card-actions>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 
 <script>
 import axios from 'axios';
-import VueJwtDecode from 'vue-jwt-decode'
+import VueJwtDecode from 'vue-jwt-decode';
 
 export default {
   name: 'Login',
@@ -55,28 +61,40 @@ export default {
       email: '',
       password: '',
       userData: null,
-      dialog: false,
+      dialog: false, rules: {
+        required: value => !!value || 'Поле обязательно для заполнения',
+        email: value => {
+          const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return pattern.test(value) || 'Введите корректный адрес электронной почты';
+        }
+      },
     };
   },
   methods: {
     async login() {
-      try {
-        const response = await axios.post(import.meta.env.VITE_API_URL + '/login', {
-          email: this.email,
-          password: this.password
-        });
-        console.log('Login successful', response);
-        localStorage.setItem('userToken', response.data.token);
-        this.$router.push('/requests')
-      } catch (error) {
-        console.error('Login failed', error);
-        alert("Ошибка входа: " + error.message);
+      if(this.email && this.password) {
+        try {
+          const response = await axios.post(`${import.meta.env.VITE_API_URL}/login`, {
+            email: this.email,
+            password: this.password,
+          });
+          console.log('Login successful', response);
+          localStorage.setItem('userToken', response.data.token);
+          const userData = VueJwtDecode.decode(localStorage.getItem('userToken'));
+          this.userData = userData.extras;
+          console.log(this.userData.status);
+          if (this.userData.status === 2) {
+            this.$router.push('/requests');
+          } else if (this.userData.status === 3) {
+            this.dialog = true;
+          }
+        } catch (error) {
+          console.error('Login failed', error);
+          alert(`Ошибка входа: ${error.message}`);
+        }
       }
     },
-    closeTab() {
-      window.close()
-    }
-  }
+  },
 };
 </script>
 
@@ -88,6 +106,3 @@ export default {
   background-size: auto 100vh;
 }
 </style>
-
-
-
